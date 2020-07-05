188d8e6381a26798dba308f51ad9f0caa4bffcaf
nami izarra <nami.izarra@gmail.com>
2020-07-06 02:21:38 +0900

    added icons, changed title, added bulma

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Button } from 'react-bulma-components';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Button color="primary">My Bulma button</Button>
     </div>
   );
 }
