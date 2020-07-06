c284b3b570c61fe5735e4494f9ea2ed7d53ed036
nami izarra <nami.izarra@gmail.com>
2020-07-07 03:51:56 +0900

    test setup

---
+++ src/__tests__/locations.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+/* Components */
+import Locations from '../components/locations'
+import Location from '../components/location'
+
+
+
